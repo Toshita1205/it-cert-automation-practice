f316a94a14c0a1ca9ae0798fde91b542c8fb78cf
toshi <toshi@gmail.com>
2020-07-11 14:16:38 +0000

    Closes: #1 Made the commitment Updated the python script

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -22,3 +22,7 @@ def validate_user(username, minlen):
 
 
 
+print(validate_user("blue.kale" , 3)) #True
+print(validate_user("%blue.kale" , 3)) # Currently true, should be false
+print(validate_user("red_quinoa", 4)) #True
+print(validate_user("&red_quinoa" , 4)) #Currently true, should be false
